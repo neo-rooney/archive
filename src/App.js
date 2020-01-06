c1964d37a0d264de5cf60900e98707affea8bdde
CheolHunBae <52039229+CHEOLHUN@users.noreply.github.com>
2020-01-06 21:29:37 +0900

    home test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component{
           <div>
             <h1>Im a class {this.state.count}</h1>
             <button onClick={this.plus}>Plus</button>
-            {/* 괄호가 없는것은 누를때만 실행, 괄호 써주면 즉시 실행 */}
+            {/* 괄호가 없는것은 누를때만 실행, 괄호 써주면 즉시 실행하는것 */}
             <button onClick={this.minus}>Minus</button>  
           </div>
     ) 
