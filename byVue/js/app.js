0c49b97c46decdd7328e14fbf71536305e8e38b1
CheolHunBae <52039229+CHEOLHUN@users.noreply.github.com>
2020-02-15 14:07:20 +0900

    build:keyword

--- byVue/js/app.js
+++ byVue/js/app.js
@@ -1,4 +1,6 @@
 import SearchModel from "./models/SearchModel.js";
+import KeywordModel from "./models/KeywordModel.js";
+import HistoryModel from "./models/HistoryModel.js";
 
 //by Rooney, 새로운 Vue 인스턴스_200214
 new Vue({
@@ -11,10 +13,14 @@ new Vue({
         submitted: false,
         tabs: ["추천 검색어", "최근 검색어"],
         selectedTab: "",
-        searchResult: []
+        searchResult: [],
+        keywords: [],
+        history: []
     },
     created() {
         this.selectedTab = this.tabs[0];
+        this.fetchKeyword();
+        this.fetchHistory();
     },
     methods: {
         //by Rooney,검색 폼에서 엔터를 누를 경우 동작하는 methods_200215
@@ -30,11 +36,31 @@ new Vue({
         onClickTab(tab) {
             this.selectedTab = tab;
         },
+        onClickKeyword(keyword) {
+            this.query = keyword;
+            this.search();
+        },
+        onClickRemoveHistory(keyword) {
+            HistoryModel.remove(keyword);
+            this.fetchHistory();
+        },
+        fetchKeyword() {
+            KeywordModel.list().then(data => {
+                this.keywords = data;
+            });
+        },
+        fetchHistory() {
+            HistoryModel.list().then(data => {
+                this.history = data;
+            });
+        },
         search() {
             SearchModel.list().then(data => {
                 this.submitted = true;
                 this.searchResult = data;
             });
+            HistoryModel.add(this.query);
+            this.fetchHistory();
         },
         //by Rooney,검색 폼에서 x 버튼을 클릭한 경우 동작하는 methods_200215
         onReset(e) {
