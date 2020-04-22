9120ef82d84d33fa2c1a71c15f8d0802b12c260f
Rooney <bch3454@gmail.com>
2020-04-22 23:45:53 +0900

    Add infinite scroll for contents

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <PostForm v-if="user" />
-    <PostCard v-for="item in content" :key="item.id" :postData="item"/>
+    <PostCard v-for="item in contents" :key="item.id" :postData="item" />
   </div>
 </template>
 
@@ -15,12 +15,36 @@ export default {
     PostForm,
     PostCard
   },
+  fetch({ store }) {
+    store.dispatch("posts/loadContents");
+  },
+  mounted() {
+    window.addEventListener("scroll", this.infinitiScroll);
+  },
+  beforeDestory() {
+    window.removeEventListener("scroll", this.infinitiScroll);
+  },
   computed: {
     user() {
       return this.$store.state.users.me;
     },
-    content() {
-      return this.$store.state.posts.content;
+    contents() {
+      return this.$store.state.posts.contents;
+    },
+    hasMoreContents() {
+      return this.$store.state.posts.hasMoreContents;
+    }
+  },
+  methods: {
+    infinitiScroll() {
+      if (
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 300
+      ) {
+        if (this.hasMoreContents) {
+          this.$store.dispatch("posts/loadContents");
+        }
+      }
     }
   }
 };
--- front/store/posts.js
+++ front/store/posts.js
@@ -1,19 +1,44 @@
 export const state = () => ({
-  content: [],
+  contents: [],
+  hasMoreContents: true,
 });
+
+const TOTAL_CONTENTS = 51;
+const LIMIT = 10;
 
 export const mutations = {
   postContent(state, payload) {
-    state.content.unshift(payload);
+    state.contents.unshift(payload);
   },
   deleteContent(state, payload) {
-    const newArr = state.content.filter((item) => item.id !== payload.id);
-    state.content = newArr;
+    const newArr = state.contents.filter((item) => item.id !== payload.id);
+    state.contents = newArr;
   },
   postComment(state, payload) {
-    const index = state.content.findIndex(item => item.id === payload.postId)
-    state.content[index].Commnets.unshift(payload)
-  }
+    const index = state.contents.findIndex(
+      (item) => item.id === payload.postId
+    );
+    state.contents[index].Commnets.unshift(payload);
+  },
+  loadContents(state, payload) {
+    const DIFF = TOTAL_CONTENTS - LIMIT;
+    const fakeContents = Array(DIFF > LIMIT ? LIMIT : DIFF)
+      .fill()
+      .map((item) => ({
+        id: Math.random().toString(),
+        content: "Hello infinite scrolling",
+        user: {
+          id: 1,
+          email: "bch3454@naver.com",
+          nickname: "rooney",
+        },
+        Commnets: [],
+        image: [],
+        createAt: Date.now(),
+      }));
+    state.contents = state.contents.concat(fakeContents);
+    state.hasMoreContents = fakeContents.length === LIMIT;
+  },
 };
 
 export const actions = {
@@ -24,6 +49,11 @@ export const actions = {
     commit("deleteContent", payload);
   },
   postCommnet({ commit }, payload) {
-    commit("postComment", payload)
-  }
+    commit("postComment", payload);
+  },
+  loadContents({ commit, state }, payload) {
+    if (state.hasMoreContents) {
+      commit("loadContents");
+    }
+  },
 };
