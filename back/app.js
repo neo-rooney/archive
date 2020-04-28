7cdf60e63480c14378364ee5d22ec5d53d6bedf7
Rooney <bch3454@gmail.com>
2020-04-28 23:19:17 +0900

    Add mysql

--- back/app.js
+++ back/app.js
@@ -1,9 +1,30 @@
 const express = require("express");
-
+const db = require("./models");
 const app = express();
 
+db.sequelize.sync();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.send("안녕 벡엔드");
+  res.send("안녕 시퀄라이즈");
+});
+
+app.post("/user", async (req, res, next) => {
+  try {
+    const newUser = await db.User.create({
+      where: {
+        email: req.body.email,
+        password: req.body.password,
+        nickname: req.body.nickname,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (err) {
+    console.err(err);
+    next(err);
+  }
 });
 
 app.listen(3085, () => {
