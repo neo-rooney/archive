9032c29fce4552fc1c013db86e6b6ecaa0b27488
Rooney <bch3454@gmail.com>
2020-07-16 23:28:30 +0900

    Set pages

---
+++ front/pages/profile.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>프로필</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ front/pages/signup.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>회원가입</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
