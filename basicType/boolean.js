e5f4bf65a34cd62c41bcb891bef53ba5e2f54cf7
cheolhun Bae <bch3454@gmail.com>
2020-08-22 00:39:09 +0900

    [1]_basic type

---
+++ basicType/boolean.js
@@ -0,0 +1,7 @@
+if (false) console.log("false");
+if (null) console.log("null");
+if (undefined) console.log("undefined");
+if (0) console.log("0");
+if ([]) console.log("empty Array");
+if ({}) console.log("empty Object");
+if ('') console.log("empty String");
