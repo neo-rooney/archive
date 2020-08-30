10c91c5884c0ea3bad8bb16503148656bf984eee
cheolhun Bae <bch3454@gmail.com>
2020-08-30 19:59:28 +0900

    Code scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app">Hello</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    fetchData(name) {
+      console.log("hello");
+    },
+  },
+};
 </script>
 
 <style>
