cab86ea999c0c46997ca10f843d03191622a7611
cheolhun Bae <bch3454@gmail.com>
2020-09-20 20:07:47 +0900

    Set load event for setting current scene

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (() => {
   let yOffset = 0; // window.pageYoOffset 대신 쓸 변수(스크롤 된 숫자 화면에 안보이는것 포함)
   let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
-  let currentScene = 0;  //현재 활성화된 씬
+  let currentScene = 0; //현재 활성화된 씬
 
   const sceneInfo = [
     {
@@ -50,35 +50,44 @@
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+    yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
-  
+
   function scrollLoop() {
-    prevScrollHeight = 0
-    for(let i=0; i < currentScene; i++){
-      prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
     }
 
-    if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       //이전 스크롤 섹션들의 높이와 현재 활성화된 씬 자기 자신의 높이의 합이 스크롤 된 높이보다 작아지면
-      //활성화된 씬을 다읍으로 넘긴다. 
-      currentScene++
+      //활성화된 씬을 다읍으로 넘긴다.
+      currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
-     if(yOffset < prevScrollHeight){
-       // 현재 스크롤된 높이가 이전 스크롤 섹션들의 높이의 합보다 작아지면 
-       // 활성화 된 씬을 이전으로 넘긴다. 
-      if(currentScene ===0) return //브라우져 바운스 효과로 인해 마이너스가 되는것 방지
-      currentScene--
+    if (yOffset < prevScrollHeight) {
+      // 현재 스크롤된 높이가 이전 스크롤 섹션들의 높이의 합보다 작아지면
+      // 활성화 된 씬을 이전으로 넘긴다.
+      if (currentScene === 0) return; //브라우져 바운스 효과로 인해 마이너스가 되는것 방지
+      currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
-
-    console.log(currentScene)
-
   }
 
   window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
-    yOffset = window.pageYOffset
+    yOffset = window.pageYOffset;
     scrollLoop();
-  })
-  setLayout();
-  window.add
+  });
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
 })();
