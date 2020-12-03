2f37ccbdddf4586a853083fd0c6139d3041c4a11
cheolhun Bae <bch3454@gmail.com>
2020-12-04 01:18:56 +0900

    Study for prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": ["eslint:recommended","eslint-config-prettier"],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "prettier"
+    ],
+    "rules": {
+        "prettier/prettier": "error"
+    },
+};
