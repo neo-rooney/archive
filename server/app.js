952ef63ef6a0630a8eec00609c6231657c30dea2
rooney <bch3454@gmail.com>
2020-12-16 19:09:17 +0900

    기능추가: Express middleware 추가
    미들웨어
    - hpp
    - helmet
    - cors
    - express.json

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
-
 import config from "./config";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 
 const { MONGO_URI } = config;
+
+app.use(hpp());
+app.use(helmet());
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
 
 mongoose
   .connect(MONGO_URI, {
