f735ebec4235764552eb47569940e49ab4377fad
rooney <bch3454@gmail.com>
2020-12-16 19:04:42 +0900

    기능추가:MongoDB 연결 및 데이터 모델링 작업
    데이터모델링
    - user
    - post
    - comment
    - category

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express()
+import config from "./config";
 
-app.get('/')
+const app = express();
+
+const { MONGO_URI } = config;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connecting Success!"))
+  .catch((e) => console.log(e));
+
+app.get("/");
 
 export default app;
