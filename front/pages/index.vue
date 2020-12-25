067442e59e6ba2e098abcb89cdf536332864372f
rooney <bch3454@gmail.com>
2020-12-26 00:06:40 +0900

    개발환경: nuxt 디렉토리 구조 설계

---
+++ front/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Home</div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "메인",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ front/pages/signup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>signup</div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "회원가입",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
