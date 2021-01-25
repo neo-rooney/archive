f9524501e95df9c65e56c3ddf4f538dc52c9f62b
rooney <bch3454@gmail.com>
2021-01-25 23:38:32 +0900

    vue-typescript 프로젝트 재 설정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <message />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import message from "@/components/message.vue";
 
-@Options({
+@Component({
   components: {
-    HelloWorld,
+    message,
   },
 })
 export default class Home extends Vue {}
