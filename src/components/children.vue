bfae9f6e01682e4e613c8158371d76297e63104f
rooney <bch3454@gmail.com>
2021-01-26 18:31:57 +0900

    props 사용방법

---
+++ src/components/children.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ parentMessage }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class Children extends Vue {
+  @Prop() parentMessage?: string;
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <message />
+    <children :parentMessage="message" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import message from "@/components/message.vue";
+import children from "@/components/children.vue";
 
 @Component({
   components: {
-    message,
+    children,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  message: string = "hellow world";
+}
 </script>
