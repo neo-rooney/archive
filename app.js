3e9849bf97119e4304e3602947d885992bd11774
rooney <bch3454@gmail.com>
2021-07-08 18:08:04 +0900

    Express / Routing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const admin = require("./routes/admin");
 
 const app = express();
 const port = 3000;
@@ -7,9 +8,7 @@ app.get("/", (req, res) => {
   res.send("hello express");
 });
 
-app.get("/user", (req, res) => {
-  res.send("hello user");
-});
+app.use("/admin", admin);
 
 app.listen(port, () => {
   console.log("Express listening on port", port);
