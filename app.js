9c8a32252f28c5eebda5e34bf16f9019e79cf30c
rooney <bch3454@gmail.com>
2021-07-13 00:00:00 +0900

    Express / middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const admin = require("./routes/admin");
 const nunjucks = require("nunjucks");
+const logger = require("morgan");
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,9 @@ nunjucks.configure("template", {
   autoescape: true,
   express: app,
 });
+
+// 미들웨어 셋팅
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.send("hello express");
