1ea0b832416212517045080ce8fbb9d3f8dd2d51
rooney <bch3454@gmail.com>
2021-07-13 21:23:07 +0900

    Express / body-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const admin = require("./routes/admin");
 const nunjucks = require("nunjucks");
 const logger = require("morgan");
+// const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
@@ -17,6 +18,8 @@ nunjucks.configure("template", {
 
 // 미들웨어 셋팅
 app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello express");
