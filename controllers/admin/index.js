7f2d21f464df59f7eec1cfaec130ebaff0b20b6d
rooney <bch3454@gmail.com>
2021-07-19 22:24:37 +0900

    feat : Set ShopsMenu Model & Create add_menu ctrl

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,21 +1,23 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const ctrl = require('./admin.ctrl');
+const ctrl = require("./admin.ctrl");
 
-router.get('/shops', ctrl.get_shops );
+router.get("/shops", ctrl.get_shops);
 
-router.get('/shops/write', ctrl.get_shops_write );
+router.get("/shops/write", ctrl.get_shops_write);
 
-router.post('/shops/write', ctrl.post_shops_write );
+router.post("/shops/write", ctrl.post_shops_write);
 
-router.get('/shops/detail/:id', ctrl.get_shops_detail );
+router.get("/shops/detail/:id", ctrl.get_shops_detail);
 
-router.get('/shops/edit/:id', ctrl.get_shops_edit );
+router.post("/shops/detail/:id", ctrl.add_menu);
 
-router.post('/shops/edit/:id', ctrl.post_shops_edit );
+router.get("/shops/detail/:shop_id/:menu_id", ctrl.remove_menu);
 
-router.get('/shops/delete/:id', ctrl.get_shops_delete );
+router.get("/shops/edit/:id", ctrl.get_shops_edit);
 
+router.post("/shops/edit/:id", ctrl.post_shops_edit);
 
+router.get("/shops/delete/:id", ctrl.get_shops_delete);
 
 module.exports = router;
