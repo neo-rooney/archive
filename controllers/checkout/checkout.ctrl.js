2aad734dc0b0016111a9d9220e865922c24a6837
rooney <bch3454@gmail.com>
2021-08-04 22:32:43 +0900

    feat : Set checkout model

---
+++ controllers/checkout/checkout.ctrl.js
@@ -0,0 +1,58 @@
+const models = require('../../models');
+
+exports.index = (req, res) => {
+    
+  let totalAmount = 0; //총결제금액
+  let cartList = {}; //장바구니 리스트
+  let shop_id = 0;
+  let menuArray = [];
+  //쿠키가 있는지 확인해서 뷰로 넘겨준다
+  if( typeof(req.cookies.cartList) !== 'undefined'){
+      //장바구니데이터
+      cartList = JSON.parse(unescape(req.cookies.cartList));
+
+      //총가격을 더해서 전달해준다.
+      for( const key in cartList){
+          totalAmount += parseInt(cartList[key].price);
+          shop_id = cartList[key].shop_id;
+          menuArray.push(parseInt(key));
+      }
+  }
+  res.render('checkout/index.html', { cartList , totalAmount , shop_id , menuArray } );
+}
+
+exports.post_complete = async (req,res) => {
+
+  try{
+    const checkout = await models.Checkout.create(req.body);
+
+    const menuArray = JSON.parse(req.body.menuArray);
+
+    async function asyncSetMenu(menu_id){
+      try{
+        const menu = await models.ShopsMenu.findByPk( menu_id );
+        const status = await checkout.addMenu(menu);
+        if(typeof status == 'undefined'){
+          throw `menu :: ${menu_id}가 존재하지 않습니다.`;
+        }
+      }catch(e){
+        throw e;
+      }
+    }
+
+    for (const menu_id of menuArray) await asyncSetMenu(menu_id);
+    
+
+    res.json({message:"success"});
+    
+  }catch(e){
+    console.log(e);
+    res.status(400).json({message: e });
+  }
+  
+  
+};
+
+exports.get_success = (req,res) => {
+  res.render('checkout/success.html');
+};
