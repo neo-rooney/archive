b552d60ce266a745d055b92ac01363fe7362fdf8
rooney <bch3454@gmail.com>
2021-12-06 23:56:05 +0900

    Baekjoon 21/12/06

---
+++ Baekjoon/10250.js
@@ -0,0 +1,33 @@
+let input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+let T = input[0];
+
+for (let i = 1; i <= T; i++) {
+  let temp = input[i].split(' ');
+
+  let H = Number(temp[0]);
+  let W = Number(temp[1]);
+  let N = Number(temp[2]);
+
+  let x = 1;
+  let y = 1;
+  let count = 1;
+
+  while (count < N) {
+    y++;
+    if (y > H) {
+      y = y === 2 ? y - 1 : y % H;
+      x++;
+    }
+    count++;
+  }
+
+  x = String(x).length < 2 ? `0${x}` : x;
+
+  let answer = Number(`${y}${x}`);
+  console.log(answer);
+}
