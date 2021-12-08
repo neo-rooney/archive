1d02fd8ba1e9074773320c3fa829bbf21b5e1e34
rooney <bch3454@gmail.com>
2021-12-08 23:35:31 +0900

    Baekjoon 21/12/08

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-let input = require('fs').readFileSync('./example.txt').toString().trim();
+let [n, ...arr] = require('fs')
+  .readFileSync('./example.txt')
+  .toString()
+  .trim()
+  .split('\n');
+arr = arr.map((item) => Number(item));
+const testCase = [];
 
-let N = Number(input);
-
-/**
-1 | 1      | 1
-2 | 234567 | 1 + 6 
-3 | 8 ~ 19 | 1 + 6 + 12
-4 | 20 ~ 37| 1 + 6 + 12 + 18
-5 | 38 ~ 61| 1 + 6 + 12 + 18 + 24
- */
-let i = 0;
-while (1) {
-  let lastNum = 1;
-  for (let j = 1; j <= i; j++) {
-    lastNum += 6 * j;
-  }
-  console.log(` i : ${i} last: ${lastNum}`);
-  if (lastNum >= N) {
-    break;
-  } else {
-    i++;
-  }
+for (let i = 0; i < n; i++) {
+  testCase[i] = arr.splice(0, 2);
 }
 
-console.log(i + 1);
+console.log(testCase);
+
+/**
+자기 앞짚과 아랫집의 합 
+ */
+for (let i = 0; i < testCase.length; i++) {
+  if (testCase[i][1] === 1) {
+    console.log(1);
+  }
+  const zero = [];
+  for (let j = 1; j < testCase[i][1]; j++) {
+    zero[i] = j;
+  }
+  console.log(zero);
+}
