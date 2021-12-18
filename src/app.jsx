fd7fab03c574716fbd0ef3182eb6b602a4266a77
rooney <rooney@rooneyui-MacBookPro.local>
2021-12-18 22:55:01 +0900

    Set app.jsx - init state - fetch youtube videos data by fetch method - set vidoes state by fetch data

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,22 @@
+import React, { useEffect, useState } from "react";
 import "./app.css";
 
 function App() {
+  const [vidoes, setVidoes] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDrX8kZjlPznZgOwCvSMPTjuE9RoMOz61A",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVidoes(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
   return <div className="App">test</div>;
 }
 
