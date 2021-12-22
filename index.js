478ba5077af6e0e5d1b3952727060070dcb0acaa
rooney <bch3454@gmail.com>
2021-12-22 23:19:51 +0900

    Inflean 21/12/22

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
-const input = fs.readFileSync("./example.txt").toString().trim();
+const input = fs.readFileSync("./example.txt").toString().trim().split("\n");
 
-let answer = "";
+const N = Number(input[0]);
+const A = input[1].split(" ").map((item) => Number(item));
+const B = input[2].split(" ").map((item) => Number(item));
 
-for (let i = 0; i < input.length; i++) {
-  const code = input[i].charCodeAt();
-  if (code >= 65 && code <= 90) {
-    answer += String.fromCharCode(code + 32);
+for (let i = 0; i < N; i++) {
+  if (A[i] === B[i]) {
+    console.log("D");
+  } else if (
+    (A[i] === 1 && B[i] === 3) ||
+    (A[i] === 2 && B[i] === 1) ||
+    (A[i] === 3 && B[i] === 2)
+  ) {
+    console.log("A");
   } else {
-    answer += String.fromCharCode(code - 32);
+    console.log("B");
   }
 }
-
-console.log(answer);
