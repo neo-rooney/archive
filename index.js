f347c8c1c811945b8f0d52a9476c35f31236e690
rooney <bch3454@gmail.com>
2022-03-14 22:15:02 +0900

    22/03/14 인프런[효율성 - 투포인터 알고리즘] 1.두배열 합치기 복습

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const fs = require("fs");
-const input = fs.readFileSync("./example.txt").toString().trim().split("\n");
+console.log(score);
 
-const N = Number(input[0]);
-const A = input[1].split(" ").map((item) => Number(item));
-const B = input[2].split(" ").map((item) => Number(item));
+score = 80;
+var score;
 
-for (let i = 0; i < N; i++) {
-  if (A[i] === B[i]) {
-    console.log("D");
-  } else if (
-    (A[i] === 1 && B[i] === 3) ||
-    (A[i] === 2 && B[i] === 1) ||
-    (A[i] === 3 && B[i] === 2)
-  ) {
-    console.log("A");
-  } else {
-    console.log("B");
-  }
-}
+console.log(score);
