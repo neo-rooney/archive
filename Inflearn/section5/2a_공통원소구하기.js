66f33621632d7061f400d5e1fa7f882069777cb7
rooney <bch3454@gmail.com>
2022-03-14 22:34:23 +0900

    22/03/14 인프런[효율성 - 투포인터 알고리즘] 2.공토원소 구하기 복습

---
+++ Inflearn/section5/2a_공통원소구하기.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+let [[N], arr_a, [M], arr_b] = fs
+  .readFileSync("./example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item) => item.split(" ").map((item) => Number(item)));
+
+let p_a = 0;
+let p_b = 0;
+arr_a = arr_a.sort((a, b) => a - b);
+arr_b = arr_b.sort((a, b) => a - b);
+
+let answer = [];
+
+while (p_a < N && p_b < M) {
+  if (arr_a[p_a] === arr_b[p_b]) {
+    answer.push(arr_a[p_a]);
+    p_a++;
+    p_b++;
+  } else if (arr_a[p_a] > arr_b[p_b]) {
+    p_b++;
+  } else {
+    p_a++;
+  }
+}
+
+console.log(answer);
