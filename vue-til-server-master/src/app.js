393e947300f14da2f4bfc8d25e05ba51036646c7
rooney <bch3454@gmail.com>
2022-04-06 11:57:04 +0900

    Connect MongoDB

--- vue-til-server-master/src/app.js
+++ vue-til-server-master/src/app.js
@@ -1,29 +1,28 @@
 // libs
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import detectPort from 'detect-port';
-import chalk from 'chalk';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import detectPort from "detect-port";
+import chalk from "chalk";
 
 // api
-import auth from './api/auth.js';
-import posts from './api/posts.js';
-import docs from './utils/api-doc.js';
+import auth from "./api/auth.js";
+import posts from "./api/posts.js";
+import docs from "./utils/api-doc.js";
 
 // utils
-import { authenticateUser } from './utils/auth.js';
+import { authenticateUser } from "./utils/auth.js";
+require("dotenv").config();
 
 // mongo db
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-mongoose.connect(
-  'mongodb+srv://test:1234@cluster0-ypgh5.mongodb.net/test?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-  },
-);
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+mongoose.connect(`${process.env.DB_KEY}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.Promise = global.Promise;
 
 // server setup
@@ -38,20 +37,20 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan('dev')); // log request
+app.use(morgan("dev")); // log request
 
 // express routers
-app.use('/', auth);
-app.use('/posts', authenticateUser, posts);
+app.use("/", auth);
+app.use("/posts", authenticateUser, posts);
 
 // api docs
-app.use('/api', docs);
+app.use("/api", docs);
 
 // start
 app.listen(port, () =>
   console.log(
     `${chalk.white
-      .bgHex('#41b883')
-      .bold(`VUE TIL SERVER IS RUNNING ON ${port}`)}`,
-  ),
+      .bgHex("#41b883")
+      .bold(`VUE TIL SERVER IS RUNNING ON ${port}`)}`
+  )
 );
