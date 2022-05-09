e2be29e692fccb472a008554aae822590928c85b
rooney <bch3454@gmail.com>
2022-05-10 00:26:33 +0900

    Study: 배경의 색, 투명도 설정하기

--- 02/basic/src/ex03.js
+++ 02/basic/src/ex03.js
@@ -1,15 +1,22 @@
 import * as THREE from "three";
 // ******************************************
-// **********브라우저 창 사이즈 변경에 대응하기***********
+// **********배경의 색 , 투명도 설정하기***********
 // ******************************************
 export default function example() {
   const canvas = document.querySelector("#three-canvas");
-  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+    alpha: true,
+  });
   renderer.setSize(window.innerWidth, window.innerHeight);
   // console.log(window.devicePixelRatio);
   renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  renderer.setClearColor("#00ff00");
+  renderer.setClearAlpha(0.5);
   // Scene
   const scene = new THREE.Scene();
+  scene.background = new THREE.Color("blue");
 
   // Camara
   const camera = new THREE.PerspectiveCamera(
