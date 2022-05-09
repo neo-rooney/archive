6e58598a8b6a00b6770185dd38cad54c577ee944
rooney <bch3454@gmail.com>
2022-05-09 22:54:12 +0900

    22/05/09 three.js 기본 요소 익히기 - 직교 카메라

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -13,9 +13,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 
 // Camara
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// camera.position.x = 1;
+// camera.position.y = 2;
+// camera.position.z = 5;
+// scene.add(camera);
+
+// 직교 카메라
+const camera = new THREE.OrthographicCamera(
+  -(window.innerWidth / window.innerHeight), //left
+  window.innerWidth / window.innerHeight, //right
+  1, // top
+  -1, //bottom,
   0.1,
   1000
 );
@@ -23,6 +38,9 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.x = 1;
 camera.position.y = 2;
 camera.position.z = 5;
+camera.lookAt(0, 0, 0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix();
 scene.add(camera);
 
 //Mesh
