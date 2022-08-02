09fea1a1020cb3f98a21a668a51b2767f6a17510
rooney <bch3454@gmail.com>
2022-08-03 00:13:44 +0900

    Feat: Created layout component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>Rooney 포트폴리오</title>
         <meta name="description" content="Rooney의 포트폴리오입니다." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>오늘도 빡코딩</h1>
-    </div>
+      <h1>홈</h1>
+    </Layout>
   )
 }
