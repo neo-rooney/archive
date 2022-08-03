90002e48f249fd85976396c2747677be1bbfad6c
rooney <bch3454@gmail.com>
2022-08-04 00:36:53 +0900

    Feat: Set eslint & prettier

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/layout'
+import Head from 'next/head';
+import Layout from '../components/layout';
 
 export default function Home() {
-  return (
-    <Layout>
-      <Head>
-        <title>Rooney 포트폴리오</title>
-        <meta name="description" content="Rooney의 포트폴리오입니다." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>홈</h1>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Head>
+				<title>Rooney 포트폴리오</title>
+				<meta name="description" content="Rooney의 포트폴리오입니다." />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1>홈</h1>
+		</Layout>
+	);
 }
