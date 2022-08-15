b910393cc2f77d33a9ca7d7ca1672a7096d71d0f
rooney <bch3454@gmail.com>
2022-08-16 01:50:10 +0900

    Feat: Changed logo at header

--- components/header.js
+++ components/header.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import DarkModeTottleButton from './dark-mode-toggle-button';
+import Image from 'next/image';
 
 export default function Header() {
 	return (
@@ -8,18 +9,17 @@ export default function Header() {
 				<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 					<Link href="/">
 						<a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								stroke="currentColor"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-								viewBox="0 0 24 24"
-							>
-								<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-							</svg>
+							<div className="w-10 h-10">
+								<Image
+									src="/logo.png"
+									width="100%"
+									height="100%"
+									layout="responsive"
+									objectFit="cover"
+									alt="logo"
+								/>
+							</div>
+
 							<span className="ml-3 text-xl">Rooney 포트폴리오</span>
 						</a>
 					</Link>
