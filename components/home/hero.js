4796e8d93549ff046f0ee20e49a95c6c2f8697c2
rooney <bch3454@gmail.com>
2022-08-16 01:05:50 +0900

    Feat: Rewrited self-introduction paragraph

--- components/home/hero.js
+++ components/home/hero.js
@@ -6,20 +6,16 @@ export default function Hero() {
 		<>
 			<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 				<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-					안녕하세요 Rooney입니다.
+					You deserve it
 					<br className="hidden lg:inline-block" />
-					오늘도 빡코딩
+					프론트엔드 개발자 배철훈입니다.
 				</h1>
 				<p className="mb-8 leading-relaxed text-justify">
-					사랑의 그것을 피어나기 인생을 위하여 풀이 눈이 때까지 것이다. 이상,
-					모래뿐일 주는 위하여서. 내는 바이며, 자신과 곧 있다. 이성은 청춘의
-					충분히 찾아다녀도, 자신과 방황하여도, 뿐이다. 우리 있는 바이며, 못할
-					끓는 풍부하게 아름다우냐? 구하지 그들의 청춘 피는 찾아 얼음이 그들의
-					인생을 얼마나 것이다. 가슴이 같이, 창공에 따뜻한 가지에 할지라도 우리
-					풀이 인간에 말이다. 두기 타오르고 살 그들은 창공에 같으며, 같은 영락과
-					사막이다. 황금시대의 눈이 천지는 두손을 구하지 역사를 이상은 가진
-					봄바람이다. 피가 뜨거운지라, 인생에 싹이 그림자는 가치를 철환하였는가?
-					황금시대를 갑 작고 찾아 대고, 할지니, 간에 대중을 수 있다.
+					3년 차 개발자로 스타트업에서 웹 서비스를 개발 / 운영 / 배포하였습니다.
+					주로 <strong>Vue</strong>, <strong>Nuxt</strong> 를 사용하여 웹
+					서비스를 개발하였으며, 일정 기간 <strong>Node.js</strong> 기반의 서버
+					개발도 겸한 경험이 있습니다. <strong>Typescript</strong>를 좋아하고,
+					<strong>테스트 자동화</strong>에 관심이 많습니다.
 				</p>
 				<div className="flex justify-center">
 					<Link href="/projects">
