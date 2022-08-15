b28b3a061575d32eb465711bb32faf7137c4d119
rooney <bch3454@gmail.com>
2022-08-15 11:30:14 +0900

    Feat: Added lottie animation at home

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,12 @@ export default function Home() {
 				<meta name="description" content="Rooney의 포트폴리오입니다." />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Hero />
+
+			<section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+				<div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+					<Hero />
+				</div>
+			</section>
 		</Layout>
 	);
 }
