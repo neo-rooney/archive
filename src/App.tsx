4cd13322c234e55d82e1a3ca7a6f2f6603f70a7d
순민 <tkksm2@gmail.com>
2022-09-10 13:20:34 +0900

    test code 동작 확인 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import CommonButton from '../src/components/CommonButton/index';
 function App() {
-  // const tasks = [
-  //   { id: 1, title: '집가고 싶다.' },
-  //   { id: 2, title: '8시 알람' },
-  // ];
   return (
     <div>
-      <CommonButton />
+      <h3>react-component</h3>
     </div>
   );
 }
