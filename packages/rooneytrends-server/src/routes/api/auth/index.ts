c1b02a8332961a10fe97dc9ad21e722e29bd3cf3
rooney <bch3454@gmail.com>
2022-09-16 01:50:17 +0900

    Fix: Cookie name

--- packages/rooneytrends-server/src/routes/api/auth/index.ts
+++ packages/rooneytrends-server/src/routes/api/auth/index.ts
@@ -48,12 +48,12 @@ function setTokenCookie(
 	reply: FastifyReply,
 	tokens: { accessToken: string; refreshToken: string },
 ) {
-	reply.setCookie('accessToken', tokens.accessToken, {
+	reply.setCookie('access_token', tokens.accessToken, {
 		httpOnly: true,
 		expires: new Date(Date.now() + 1000 * 60 * 60),
 		path: '/',
 	});
-	reply.setCookie('refreshToken', tokens.refreshToken, {
+	reply.setCookie('refresh_token', tokens.refreshToken, {
 		httpOnly: true,
 		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
 		path: '/',
