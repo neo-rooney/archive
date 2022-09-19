b91f2cf903b9945df2c443c88356cd79a05fbfc4
권순민 <tkksm@blocko.io>
2022-09-19 13:40:01 +0900

    slider components styling

--- src/SlidePopUp/SliderPopUp.styles.ts
+++ src/SlidePopUp/SliderPopUp.styles.ts
@@ -1,15 +1,102 @@
 import styled, { css } from 'styled-components';
 
+interface popUpStyle {
+  isActive?: boolean;
+}
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 30px;
+`;
+
+export const MainWrapper = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  justify-content: flex-start;
+  width: 300px;
+  height: 373px;
+  overflow-x: hidden;
+`;
+
 export const ContentsWrapper = styled.div`
+  flex-shrink: 0;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-flow: nowrap column;
   gap: 30px;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-  padding-bottom: 10px;
 `;
 
 export const ImageWrapper = styled.img`
   width: 300px;
   height: 300px;
+`;
+
+export const Description = styled.article`
+  font-size: 18px;
+  text-align: center;
+`;
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 10px;
+`;
+
+export const PaginationWrapper = styled.ul`
+  padding: 0;
+  margin: 0;
+  display: inline-flex;
+  gap: 8px;
+`;
+
+export const PaginationList = styled.li`
+  list-style: none;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: red;
+  background-color: #d9d9d9;
+  border-radius: 38.5px;
+  &:hover {
+    cursor: pointer;
+  }
+  ${({ isActive }: popUpStyle) =>
+    isActive &&
+    css`
+      background-color: #5170bb;
+      width: 40px;
+      height: 10px;
+    `}
+`;
+
+export const ButtonWrapper = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+  border: none;
+  background-color: inherit;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+export const ButtonLeft = styled(Button)`
+  color: #a0a0a0;
+`;
+
+export const ButtonRight = styled(Button)`
+  background-color: #5170bb;
+  color: #e6eaf1;
+  border-radius: 38.5px;
+  width: 120px;
+  height: 48px;
 `;
--- src/SlidePopUp/SliderPopUp.tsx
+++ src/SlidePopUp/SliderPopUp.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import * as Styled from './SliderPopUp.styles';
 import * as CommonStyled from '../Common/ModalFrame.styles';
 import theme from '../theme';
-import image1 from './';
 
-const SliderPopUp = () => {
+interface popUpContentsType {
+  popUpContents: {
+    imgSrc: string;
+    contents: string;
+  }[];
+}
+const SliderPopUp: React.FC<popUpContentsType> = ({ popUpContents }) => {
+  const [count, setCount] = useState<number>(0);
   return (
     <ThemeProvider theme={theme}>
       <CommonStyled.Overlay>
-        <CommonStyled.Container pt={51}>
-          <Styled.ContentsWrapper>
-            <Styled.ImageWrapper src={image1} alt={'logo'} />
-          </Styled.ContentsWrapper>
+        <CommonStyled.Container pt={51} pl={24} pr={24} pb={24}>
+          <Styled.MainContainer>
+            <Styled.MainWrapper>
+              {popUpContents.map((item) => (
+                <Styled.ContentsWrapper>
+                  <Styled.ImageWrapper src={item.imgSrc} alt={'logo'} />
+                  <Styled.Description
+                    dangerouslySetInnerHTML={{ __html: item.contents }}
+                  ></Styled.Description>
+                </Styled.ContentsWrapper>
+              ))}
+            </Styled.MainWrapper>
+          </Styled.MainContainer>
+          <Styled.PaginationContainer>
+            <Styled.PaginationWrapper>
+              <Styled.PaginationList isActive></Styled.PaginationList>
+              <Styled.PaginationList></Styled.PaginationList>
+              <Styled.PaginationList></Styled.PaginationList>
+            </Styled.PaginationWrapper>
+          </Styled.PaginationContainer>
+          <Styled.ButtonWrapper>
+            <Styled.ButtonLeft>그만 볼게요.</Styled.ButtonLeft>
+            <Styled.ButtonRight>다음</Styled.ButtonRight>
+          </Styled.ButtonWrapper>
         </CommonStyled.Container>
       </CommonStyled.Overlay>
     </ThemeProvider>
