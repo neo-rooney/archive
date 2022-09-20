e9ad84506a114087785a1fed4cf43465dce39c2d
rooney <bch3454@gmail.com>
2022-09-20 17:24:59 +0900

    Create declaration

---
+++ dist/types/Colors/colors.d.ts
@@ -0,0 +1,14 @@
+export declare const colors: {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+    warning: string;
+    green: string;
+    orange: string;
+    gray0: string;
+    gray1: string;
+    gray2: string;
+    gray3: string;
+    gray4: string;
+    gray5: string;
+};
