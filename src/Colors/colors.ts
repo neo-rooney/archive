c783b1fbf4bc014b381188ead5f980824825b65d
rooney <bch3454@gmail.com>
2022-09-20 13:52:09 +0900

    Feat: Add Colors.mdx

---
+++ src/Colors/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: "#7689B7",
+  secondary: "#5170BB",
+  tertiary: "#99B7FF",
+  warning: "#FF7272",
+  green: "#77EC98",
+  orange: "#FF9966",
+  gray0: "#ECECEC",
+  gray1: "#D9D9D9",
+  gray2: "#A0A0A0",
+  gray3: "#4B4B4B",
+  gray4: "#252525",
+  gray5: "#121212",
+};
