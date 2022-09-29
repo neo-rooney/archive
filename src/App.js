4d0ac117edfb791da0a19577a6b61d8f0b94bef5
rooney <bch3454@gmail.com>
2022-09-29 18:57:56 +0900

    NewsItemBlock 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-
+import { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  return <div></div>;
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const res = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=66e45ce613374adeaae5b4eab34c0eff',
+      );
+      setData(res.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 };
 
 export default App;
