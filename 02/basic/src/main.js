3e3a49f81e3c993bcdcca6aea5726a99216df988
rooney <bch3454@gmail.com>
2022-10-10 15:03:16 +0900

    기본 장면 만들기 - 직교 카메라

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -22,20 +22,42 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 
 /**
- * Camera
+ * Camera(PerspectiveCamera)
+ * PerspectiveCamera를 많이 씀
  */
-const camera = new THREE.PerspectiveCamera(
-  75, // 시야각
-  window.innerWidth / window.innerHeight, // 종횡비
+// const camera = new THREE.PerspectiveCamera(
+//   75, // 시야각
+//   window.innerWidth / window.innerHeight, // 종횡비
+//   0.1, // near
+//   1000 // far
+// );
+// // 카메라의 위치
+// camera.position.x = 1;
+// camera.position.y = 2;
+// camera.position.z = 5;
+// scene.add(camera);
+
+/**
+ * Camera(OrthographicCamera)
+ * 직교 카메라, 원근에 따라서 물체의 크기가 다르지 않음
+ * 대표적으로 디아블로, 롤 카메라
+ */
+const camera = new THREE.OrthographicCamera(
+  -(window.innerWidth / window.innerHeight), // left
+  window.innerWidth / window.innerHeight, // right
+  1, // top
+  -1, // bottom
   0.1, // near
   1000 // far
 );
-// 카메라의 위치
+
 camera.position.x = 1;
 camera.position.y = 2;
 camera.position.z = 5;
+camera.lookAt(0, 0, 0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix();
 scene.add(camera);
-
 /**
  * Mesh
  * Geometry + Material
