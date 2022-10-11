3e882230b9456cb6ddf813297dcd5a98dc034fa0
rooney <bch3454@gmail.com>
2022-10-11 19:38:34 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import { createStore } from "redux";
+
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const bunIncrease = document.querySelector("#increase");
+const bunDecrease = document.querySelector("#decrease");
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = () => ({ type: INCREASE });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
