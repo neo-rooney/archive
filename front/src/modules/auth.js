eb8b8f303a01681cbfa622f817ee515b2f2da8de
rooney <bch3454@gmail.com>
2022-10-16 23:13:32 +0900

    redux saga 적용 중

--- front/src/modules/auth.js
+++ front/src/modules/auth.js
@@ -1,8 +1,15 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { createRequestActionTypes } from '../lib/createRequestSage';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISER, REGISTER_SUCCESS, REGISTER_FAILURE] =
+  createRequestActionTypes('auth/REGISTER');
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] =
+  createRequestActionTypes('auth/LOGIN');
 
 export const changeField = createAction(
   CHANGE_FIELD,
