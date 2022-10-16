555d7a1938eb7e662a1ff580fdd5ffd3ac44d18a
rooney <bch3454@gmail.com>
2022-10-16 18:55:17 +0900

    auth api 작업

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 import api from './api/index.js';
 import dotenv from 'dotenv';
 import createFakeData from './createFakeData.js';
-
+import jwtMiddleware from './lib/jwtMiddleware.js';
 dotenv.config();
 
 const { PORT, MONGO_URL } = process.env;
@@ -28,6 +28,7 @@ router.use('/api', api.routes());
 
 // 라우터 적용하기 전에 bodyParser 적용
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
