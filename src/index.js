81a4f77bb809ef7dfc4dd891f43c0dfd5745c241
rooney <bch3454@gmail.com>
2022-10-21 17:31:54 +0900

    미들웨어 학습

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import loggerMiddleware from "./lib/loggerMiddleware";
+import logger from "redux-logger";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(rootReducer, applyMiddleware(logger));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
