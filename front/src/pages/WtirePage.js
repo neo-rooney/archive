fc31bf49f4f2cb178afc93e36ecd93b9102720d9
rooney <bch3454@gmail.com>
2022-10-25 23:57:21 +0900

    Tag 등록 작업

--- front/src/pages/WtirePage.js
+++ front/src/pages/WtirePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 
 const WtirePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
     </Responsive>
   );
 };
