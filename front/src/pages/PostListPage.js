f921e172ceb576c673f594809f57fdb18a56d2d0
rooney <bch3454@gmail.com>
2022-10-27 18:56:45 +0900

    post list ui

--- front/src/pages/PostListPage.js
+++ front/src/pages/PostListPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Button from '../components/common/Button';
-import Header from '../components/common/Header';
+import PostList from '../components/posts/PostList';
 import HeaderContainer from '../containers/common/HeaderContainer';
 
 const PostListPage = () => {
   return (
-    <div>
+    <>
       <HeaderContainer />
-    </div>
+      <PostList />
+    </>
   );
 };
 
