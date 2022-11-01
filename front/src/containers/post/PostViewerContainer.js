f6cbef7256a6864c2d972a389d7e8a098999f178
rooney <bch3454@gmail.com>
2022-11-01 12:17:50 +0900

    포스트 수정

--- front/src/containers/post/PostViewerContainer.js
+++ front/src/containers/post/PostViewerContainer.js
@@ -3,14 +3,22 @@ import PostViewer from '../../components/post/PostViewer';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { readPost, unloadPost } from '../../modules/post';
+import PostActionButtons from '../../components/post/PostActionButtons';
+import { setOriginalPost } from '../../modules/write';
+import { useNavigate } from 'react-router-dom';
+import user from '../../modules/user';
+
 const PostViewerContainer = () => {
   const { postId } = useParams();
   const dispatch = useDispatch();
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-  }));
+  const { post, error, loading, user } = useSelector(
+    ({ post, loading, user }) => ({
+      post: post.post,
+      error: post.error,
+      loading: loading['post/READ_POST'],
+      user: user.user,
+    }),
+  );
 
   useEffect(() => {
     dispatch(readPost(postId));
@@ -18,8 +26,22 @@ const PostViewerContainer = () => {
       dispatch(unloadPost());
     };
   }, [dispatch, postId]);
+  const navigate = useNavigate();
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    navigate('/write');
+  };
 
-  return <PostViewer post={post} loading={loading} error={error} />;
+  const ownPost = (user && user._id) === (post && post.user._id);
+
+  return (
+    <PostViewer
+      post={post}
+      loading={loading}
+      error={error}
+      actionButtons={ownPost && <PostActionButtons onEdit={onEdit} />}
+    />
+  );
 };
 
 export default PostViewerContainer;
--- front/src/containers/write/WriteActionButtonContainer.js
+++ front/src/containers/write/WriteActionButtonContainer.js
@@ -1,20 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { writePost } from '../../modules/write';
+import { updatePost, writePost } from '../../modules/write';
 import { useNavigate } from 'react-router-dom';
 import WriteActionButton from '../../components/write/WriteActionButton';
 
 const WriteActionButtonContainer = () => {
   const dispatch = useDispatch();
-  const { title, body, tags, post, postError } = useSelector(({ write }) => ({
-    title: write.title,
-    body: write.body,
-    tags: write.tags,
-    post: write.post,
-    postError: write.postError,
-  }));
+  const { title, body, tags, post, postError, origianlPostId } = useSelector(
+    ({ write }) => ({
+      title: write.title,
+      body: write.body,
+      tags: write.tags,
+      post: write.post,
+      postError: write.postError,
+      origianlPostId: write.origianlPostId,
+    }),
+  );
 
   const onPublish = () => {
+    if (origianlPostId) {
+      dispatch(updatePost({ title, body, tags, id: origianlPostId }));
+      return;
+    }
     dispatch(
       writePost({
         title,
@@ -38,7 +45,13 @@ const WriteActionButtonContainer = () => {
     }
   }, [post, postError, navigate]);
 
-  return <WriteActionButton onPublish={onPublish} onCancel={onCancel} />;
+  return (
+    <WriteActionButton
+      onPublish={onPublish}
+      onCancel={onCancel}
+      isEdit={!!origianlPostId}
+    />
+  );
 };
 
 export default WriteActionButtonContainer;
