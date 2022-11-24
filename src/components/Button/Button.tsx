db63e4a6bb6fa6b3cd5015d2319d9097c53e8746
rooney <bch3454@gmail.com>
2022-11-24 18:13:43 +0900

    Feat: Mui 버튼 컴포넌트 제작

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from "@mui/material";
 
-export interface ButtonProps {
+type ButtonBaseProps = Pick<MuiButtonProps, "variant" | "size" | "color">;
+
+export interface ButtonProps extends ButtonBaseProps {
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
-};
+const Button = ({ label, ...rest }: ButtonProps) => (
+  <MuiButton {...rest}>{label}</MuiButton>
+);
 
 export default Button;
