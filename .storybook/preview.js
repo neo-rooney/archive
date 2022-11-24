8a688ad40daf46b353e506fdc608d781378925c2
rooney <bch3454@gmail.com>
2022-11-24 11:19:16 +0900

    Feat: Initial mui & roboto font & meterial-icons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/material-icons";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +12,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
