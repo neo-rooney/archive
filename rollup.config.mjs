b2c2e84b9f31d6952bc0109b90b9391e49e644a0
rooney <bch3454@gmail.com>
2022-11-30 12:05:11 +0900

    Feat: Extracted plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,26 @@
 import babel from "@rollup/plugin-babel";
 import typescript from "@rollup/plugin-typescript";
+import { getFolders } from "./scripts/buildUtils.mjs";
 
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
+
+const plugins = [
+  // 바벨 트랜스파일러 설정
+  babel({
+    babelHelpers: "bundled",
+    // presets에 typescript 추가
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+    ],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  }),
+  // 타입스크립트
+  typescript({ tsconfig: "./tsconfig.json" }),
+];
 
 export default {
   input: "./src/index.ts",
@@ -19,19 +36,5 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [
-    // 바벨 트랜스파일러 설정
-    babel({
-      babelHelpers: "bundled",
-      // presets에 typescript 추가
-      presets: [
-        "@babel/preset-env",
-        "@babel/preset-react",
-        "@babel/preset-typescript",
-      ],
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    }),
-    // 타입스크립트
-    typescript({ tsconfig: "./tsconfig.json" }),
-  ],
+  plugins,
 };
